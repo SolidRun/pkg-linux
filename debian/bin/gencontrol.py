3e036f1ff42e0f3f7b069877a99e1514d96a17ee
Bastian Blank <waldi@debian.org>
2006-08-20 11:48:38 +0000

    debian/bin/gencontrol.py: Remove hacks.
    svn path=/dists/sid/linux-latest-2.6/; revision=7204

--- debian/bin/gencontrol.py
+++ debian/bin/gencontrol.py
@@ -33,7 +33,7 @@ class gencontrol(debian_linux.gencontrol.gencontrol):
                 package['Architecture'] = [arch]
                 packages.append(package)
 
-        makeflags['GENCONTROL_ARGS'] = '-v%s' % self.version['source']
+        makeflags['GENCONTROL_ARGS'] = '-v%s' % self.package_version
         makeflags_string = ' '.join(["%s='%s'" % i for i in makeflags.iteritems()])
 
         cmds_binary_arch = []
@@ -59,63 +59,20 @@ class gencontrol(debian_linux.gencontrol.gencontrol):
             for i in extra_arches[arch]:
                 tmp = []
                 if i.has_key('X-Version-Overwrite-Epoch'):
-                        tmp.append("-v1:%s" % self.version['source'])
+                        tmp.append("-v1:%s" % self.package_version)
                 cmds.append("$(MAKE) -f debian/rules.real install-dummy ARCH='%s' DH_OPTIONS='-p%s' GENCONTROL_ARGS='%s'" % (arch, i['Package'], ' '.join(tmp)))
             makefile.append("binary-arch-%s:: binary-arch-%s-extra" % (arch, arch))
             makefile.append(("binary-arch-%s-extra:" % arch, cmds))
 
     def process_changelog_version(self):
         changelog_version = read_changelog()[0]['Version']
-        # HACKALARM
-        self.version['source'] = '%s+%s' % (self.version['upstream'], changelog_version)
+        self.package_version = '%s+%s' % (self.version['upstream'], changelog_version['complete'])
 
     def process_config_version(self, config):
         entry = config['version',]
-        self.process_version(parse_version(entry['source']))
-        self.vars['abiname'] = self.abiname = entry['abiname']
-
-# HACKALARM
-def read_changelog(dir = ''):
-    r = re.compile(r"""
-^
-(
-(?P<header>
-    (?P<header_source>
-        \w[-+0-9a-z.]+
-    )
-    \ 
-    \(
-    (?P<header_version>
-        [^\(\)\ \t]+
-    )
-    \)
-    \s+
-    (?P<header_distribution>
-        [-0-9a-zA-Z]+
-    )
-    \;
-)
-)
-""", re.VERBOSE)
-    f = file(os.path.join(dir, "debian/changelog"))
-    entries = []
-    act_upstream = None
-    while True:
-        line = f.readline()
-        if not line:
-            break
-        line = line.strip('\n')
-        match = r.match(line)
-        if not match:
-            continue
-        if match.group('header'):
-            e = {}
-            e['Distribution'] = match.group('header_distribution')
-            e['Source'] = match.group('header_source')
-            e['Version'] = match.group('header_version')
-            entries.append(e)
-            break
-    return entries
+        self.version = parse_version(entry['source'])
+        self.abiname = entry['abiname']
+        self.vars = self.process_version_linux(self.version, self.abiname)
 
 if __name__ == '__main__':
     gencontrol(sys.argv[1] + "/arch")()
