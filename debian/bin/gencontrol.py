a97edfad05b2c15b8bcad2d4a77adcf82e60adf6
Bastian Blank <waldi@debian.org>
2008-01-31 18:01:45 +0000

    * debian/bin/gencontrol.py: Adopt changes. * debian/changelog: Update * debian/rules.defs: Update to 2.6.24-1.
    svn path=/dists/sid/linux-latest-2.6/; revision=10344

--- debian/bin/gencontrol.py
+++ debian/bin/gencontrol.py
@@ -1,23 +1,32 @@
 #!/usr/bin/env python2.4
+
 import sys
-sys.path.append(sys.argv[1]+ "/lib/python")
+sys.path.append(sys.argv[1] + "/lib/python")
+
+from debian_linux.config import ConfigCoreDump
+from debian_linux.debian import Changelog, VersionLinux
 from debian_linux.gencontrol import Gencontrol as Base
-from debian_linux.config import *
-from debian_linux.debian import *
+from debian_linux.utils import Templates
 
 class Gencontrol(Base):
     def __init__(self, config):
-        super(Gencontrol, self).__init__(config)
-        self.process_config_version(ConfigParser({}, [sys.argv[1] + "/version"]))
-        self.process_changelog_version()
+        super(Gencontrol, self).__init__(ConfigCoreDump(fp = file(config)), Templates(["debian/templates"]))
+
+        config_entry = self.config['version',]
+        self.version = VersionLinux(config_entry['source'])
+        self.abiname = config_entry['abiname']
+        self.vars = self.process_version_linux(self.version, self.abiname)
+
+        changelog_version = Changelog()[0].version
+        self.package_version = '%s+%s' % (self.version.upstream, changelog_version.complete)
 
     def do_main_packages(self, packages, extra):
         packages['source']['Build-Depends'].extend(
             ['linux-support-%s%s' % (self.version.linux_upstream, self.abiname)]
         )
 
-    def do_flavour_packages(self, packages, makefile, arch, subarch, flavour, vars, makeflags, extra):
-        config_entry = self.config.merge('base', arch, subarch, flavour)
+    def do_flavour_packages(self, packages, makefile, arch, featureset, flavour, vars, makeflags, extra):
+        config_entry = self.config.merge('base', arch, featureset, flavour)
 
         templates = []
 
@@ -40,12 +49,9 @@ class Gencontrol(Base):
                 packages.append(package)
 
         makeflags['GENCONTROL_ARGS'] = '-v%s' % self.package_version
-        makeflags_string = ' '.join(["%s='%s'" % i for i in makeflags.iteritems()])
 
-        cmds_binary_arch = []
-        cmds_binary_arch.append(("$(MAKE) -f debian/rules.real install-dummy DH_OPTIONS='%s' %s" % (' '.join(["-p%s" % i['Package'] for i in packages_dummy]), makeflags_string),))
-        makefile.append(("binary-arch-%s-%s-%s-real:" % (arch, subarch, flavour), cmds_binary_arch))
-        makefile.append(("build-%s-%s-%s-real:" % (arch, subarch, flavour)))
+        cmds_binary_arch = ["$(MAKE) -f debian/rules.real install-dummy DH_OPTIONS='%s' %s" % (' '.join(["-p%s" % i['Package'] for i in packages_dummy]), makeflags)]
+        makefile.add('binary-arch_%s_%s_%s_real' % (arch, featureset, flavour), cmds = cmds_binary_arch)
 
     def do_extra(self, packages, makefile):
         templates_extra = self.templates["control.extra"]
@@ -68,18 +74,8 @@ class Gencontrol(Base):
                 else:
                     version = '-v%s' % self.package_version
                 cmds.append("$(MAKE) -f debian/rules.real install-dummy ARCH='%s' DH_OPTIONS='-p%s' GENCONTROL_ARGS='%s'" % (arch, i['Package'], version))
-            makefile.append("binary-arch-%s:: binary-arch-%s-extra" % (arch, arch))
-            makefile.append(("binary-arch-%s-extra:" % arch, cmds))
-
-    def process_changelog_version(self):
-        changelog_version = Changelog()[0].version
-        self.package_version = '%s+%s' % (self.version.upstream, changelog_version.complete)
-
-    def process_config_version(self, config):
-        entry = config['version',]
-        self.version = VersionLinux(entry['source'])
-        self.abiname = entry['abiname']
-        self.vars = self.process_version_linux(self.version, self.abiname)
+            makefile.add('binary-arch_%s' % arch, ['binary-arch_%s_extra' % arch])
+            makefile.add("binary-arch_%s_extra" % arch, cmds = cmds)
 
 if __name__ == '__main__':
-    Gencontrol(sys.argv[1] + "/arch")()
+    Gencontrol(sys.argv[1] + "/config.defines.dump")()
