3aba69f0caffeedf0ecc591fa14d6f00a52ce3f2
Ben Hutchings <benh@debian.org>
2010-07-06 00:21:23 +0000

    Change binary-indep versions to match binary-arch versions
    This unfortunately requires adding an epoch to the linux-doc-2.6 and
    linux-source-2.6 packages.
    
    svn path=/dists/sid/linux-latest-2.6/; revision=15952

--- debian/bin/gencontrol.py
+++ debian/bin/gencontrol.py
@@ -26,6 +26,9 @@ class Gencontrol(Base):
         changelog_version = Changelog()[0].version
         self.package_version = '%s+%s' % (self.version.upstream, changelog_version.complete)
 
+    def do_main_setup(self, vars, makeflags, extra):
+        makeflags['GENCONTROL_ARGS'] = '-v%s' % self.package_version
+
     def do_main_packages(self, packages, vars, makeflags, extra):
         packages['source']['Build-Depends'].extend(
             ['linux-support-%s%s' % (self.version.linux_upstream, self.abiname)]
